ec8a9b872c9172d0b5b2443efdc0e051455d3eca
AmanNinave <amanninave987@gmail.com>
2024-05-06 02:37:27 +0530

    private route

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToDatabase from "./db/connectToDatabase.js";
 
 dotenv.config();
@@ -9,9 +11,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000 ;
 
-app.use(express.json());      // to parse incomming requests with JSON payloads ( from req.body );
-
+app.use(express.json());                    // to parse incomming requests with JSON payloads ( from req.body );
+app.use(cookieParser());
 app.use("/api/auth" , authRoutes );
+app.use("/api/message" , messageRoutes );
 
 app.get("/" , (req , res )=>{
     res.send("home page is here");
