1521e0c23933b5d73601c59fbcf7124e07848f82
AmanNinave <amanninave987@gmail.com>
2024-05-05 18:50:08 +0530

    signup done

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,12 +1,60 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
+
+export const signUpUser = async ( req , res)=>{
+    try{
+        const { fullName, username , password , confirmPassword , gender } = req.body;
+
+        if(password !== confirmPassword ){
+            return res.status(400).json({error : "password don't match"})
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json({error : "username already exist"});
+        }
+
+        // Hash password here
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password , salt );
+
+        // https://avatar.iran.liara.run/public/boy?username=Aman
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password : hashedPassword ,
+            gender,
+            profilePic : gender === "male" ? boyProfilePic : girlProfilePic 
+        })
+
+        if( newUser ){                               // If user gives wrong data
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName : newUser.fullName,
+                username: newUser.username,
+                profilePic : newUser.profilePic
+            })
+        }else{
+            res.status(400).json({error : "Invalid user data"});
+        }
+        
+    }catch(error){
+        console.log("Error in signup controller", error.message );
+        res.status(500).json({ error : "Internal Server Error"});
+    }
+}
+
 export const loginUser = ( req , res)=>{
     console.log("login user");
     res.send("login");
-}
-
-
-export const signUpUser = ( req , res)=>{
-    console.log("signup user");
-    res.send("signup ");
 }
 
 export const logoutUser = ( req , res)=>{
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js"
-const app = express();
+import connectToDatabase from "./db/connectToDatabase.js";
 
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 5000 ;
 
+app.use(express.json());      // to parse incomming requests with JSON payloads ( from req.body );
+
+app.use("/api/auth" , authRoutes );
 
 app.get("/" , (req , res )=>{
     res.send("home page is here");
 }) 
 
-app.use("/api/auth" , authRoutes );
 
-app.listen( PORT , ()=>{
-    console.log("Server running on " + PORT );
+connectToDatabase().then(()=>{
+    app.listen( PORT , ()=>{
+        console.log("Server running on " + PORT );
+    })
 })
