a1092e7063468ed23a10cd914f2ea602865d7185
AmanNinave <amanninave987@gmail.com>
2024-05-09 01:30:57 +0530

    Implemented sendMessages & getMessages Functionalities

--- Backend/controllers/message.controller.js
+++ Backend/controllers/message.controller.js
@@ -1,13 +1,63 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
 export const sendMessage = async (req , res )=>{
    try {
-     const {id} = req.params;
      const {message } = req.body;
+     const {id: receiverId } = req.params;
      const senderId = req.user._id;
 
+     let conversation  = await Conversation.findOne({
+      participants: {$all : [ senderId , receiverId ]},
+     })
 
+     if(!conversation){
+      conversation = await Conversation.create({
+        participants : [senderId , receiverId ]
+      })
+     }
+
+     const newMessage = new Message({
+      senderId,
+      receiverId, 
+      message
+     })
+
+     if(newMessage){
+      conversation.messages.push(newMessage._id);
+     }
+
+     // SOCKET IO FUNCTIONALITY HERE
+
+
+    //  await conversation.save();
+    //  await newMessage.save();
+
+    await Promise.all([conversation.save() , newMessage.save()]);  // This will run in parallel
+
+     res.status(201).json(newMessage);
+    
    } catch (error) {
     console.log("Error from sendMessage controller " + error.message );
     res.status(500).json({ error : "Internal server error" });
    }
+}
+
+
+export const getMessages = async (req , res )=> {
+  try {
+
+    const {id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants : { $all : [senderId , userToChatId ]}
+    }).populate("messages");
+
+    res.status(200).json(conversation.messages );
+
+  }catch (error){
+    console.log("Error in getMessages controller " , error.message );
+    res.send(500).json({error : "Internal server error"});
+  }
 }
--- Backend/middleware/protectRoute.js
+++ Backend/middleware/protectRoute.js
@@ -11,11 +11,11 @@ const protectRoute = async (req , res , next ) =>{
 
         const decoded = jwt.verify(token , process.env.JWT_SECRET );
 
-        if(!decoded){
+        if( !decoded ){
             return res.status(401).json({error : "Unauthorized - Invalid Token"})
         }
 
-        const user = await User.findById(decoded.userId).select("-password");
+        const user = await User.findById(decoded.userID).select("-password");
 
         if(!user){
             return res.status(404).json({error : "User not found"});
