350bd9561b33f14fec47ed8003a138365239445c
AmanNinave <amanninave987@gmail.com>
2024-05-09 01:53:18 +0530

    getUsers Implementation

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { getUsersForSidebar } from '../controllers/user.controllers.js';
+
+const router = express.Router();
+
+
+router.get("/" , protectRoute , getUsersForSidebar );
+
+
+
+export default router;
